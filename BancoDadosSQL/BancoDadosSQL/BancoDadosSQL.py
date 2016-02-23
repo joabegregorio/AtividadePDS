b1d655eaf8efe33aca96d621fb3cc8029f5a894b
VitorRamos <ramos.vitor89@gmail.com>
2016-02-23 16:39:14 -0300

    teste conexão banco

--- BancoDadosSQL/BancoDadosSQL/BancoDadosSQL.py
+++ BancoDadosSQL/BancoDadosSQL/BancoDadosSQL.py
@@ -1,2 +1,12 @@
-import mysql
+import mysql.connector
 
+cnn=mysql.connector.connect(user="main",password="123",host="localhost",database="sys")
+
+cur=cnn.cursor()
+
+cur.execute("SELECT * FROM nomes")
+
+data= cur.fetchall()
+
+for x in data :
+    print(x)
