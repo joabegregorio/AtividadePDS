9076385ad14cbe7509d46f60100d1f5ece4316a2
VitorRamos <ramos.vitor89@gmail.com>
2016-02-23 16:54:54 -0300

    menu inicial

--- BancoDadosSQL/BancoDadosSQL.py
+++ BancoDadosSQL/BancoDadosSQL.py
@@ -4,20 +4,22 @@ cnn= mysql.connector.connect(user="root",password="katrina",host="localhost",
                      database="sys")
 cur=cnn.cursor()
 
+print("Escolha a operacao : ")
+print("1 - Ler : ")
+print("2 - Escrever : ")
 
-print("Digite a entrada : ")
-nome= raw_input("Nome: ")
-escola= raw_input("Escola: ")
-idade= raw_input("Idade: ")
+op= input()
 
-cur.execute("INSERT INTO nomes (nome, escola, idade) VALUES (%s, %s, %s)",
-            (nome, escola, idade))
+if op == 1:
+    cur.execute("SELECT * FROM nomes")
+    data= cur.fetchall()
+    for x in data:
+        print(x)
 
-cnn.commit()
-
-cur.execute("SELECT * FROM nomes")
-
-data= cur.fetchall()
-
-for x in data:
-    print(x)
+if op == 2: 
+    nome= raw_input("Nome: ")
+    escola= raw_input("Escola: ")
+    idade= raw_input("Idade: ")
+    cur.execute("INSERT INTO nomes (nome, escola, idade) VALUES (%s, %s, %s)",
+                (nome, escola, idade))
+    cnn.commit()
