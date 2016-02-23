7dd5d932296262b6f3c280912d19351fd264b077
VitorRamos <ramos.vitor89@gmail.com>
2016-02-23 16:49:10 -0300

    projeto inicial

--- BancoDadosSQL/BancoDadosSQL.py
+++ BancoDadosSQL/BancoDadosSQL.py
@@ -1,13 +1,23 @@
 import mysql.connector
 
-cnn=mysql.connector.connect(user="main",password="123",host="localhost",database="sys")
-
+cnn= mysql.connector.connect(user="root",password="katrina",host="localhost",
+                     database="sys")
 cur=cnn.cursor()
+
+
+print("Digite a entrada : ")
+nome= raw_input("Nome: ")
+escola= raw_input("Escola: ")
+idade= raw_input("Idade: ")
+
+cur.execute("INSERT INTO nomes (nome, escola, idade) VALUES (%s, %s, %s)",
+            (nome, escola, idade))
+
+cnn.commit()
 
 cur.execute("SELECT * FROM nomes")
 
 data= cur.fetchall()
 
-for x in data :
+for x in data:
     print(x)
-
